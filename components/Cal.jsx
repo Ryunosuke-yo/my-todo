3671160f84a13c025bd139e9f428604e399cf6e4
Ryunosuke Yokokawa <ryokokawa00mylangara.ca>
2022-01-08 23:00:27 -0800

    今日はもう無理

--- components/Cal.jsx
+++ components/Cal.jsx
@@ -7,40 +7,63 @@ import { Ellipse } from 'react-native-svg';
 const Cal = ({todos, addDue, current}) => {
     const [dueList, setDueLIst] = useState([])
     const [ob, setOb] = useState({})
+    const [dIndex, setDIndex] = useState([])
 
     useEffect(()=>{
         
         
-        const integrateDue = ([...array])=>{
-        if (array.length > 0) {
-                const count = array.filter(el=> el.date === current).length
-                if(count > 1){
-                    const mapped = array.map((el, i, self)=>{
-                        const index = self.findIndex((el)=>el.date === current)
-                        el.dotList.dots.push(self[index].dotList.dots[0])
+        // const integrateDue = ([...array])=>{
+        // if (array.length > 0) {
+        //         const count = array.filter(el=> el.date === current).length
+        //         if(count > 1){
+        //             const indexes = ()=>{for (let i = 0; i < array.length; i++) {
+        //                 if(array[i].dotList.dots == current){
+        //                     return i
+        //                 }
+        //             }
+                        
+        //             }
+        //             const mapped = array.map((el, i, self)=>{
+        //                 const index = self.findIndex((el)=>el.date === current)
+        //                 el.dotList.dots.push(self[index].dotList.dots[0])
                        
-                        return {
-                            dotList: el.dotList,
-                            date : el.date,
-                            id : el.id
-                        }
-                    }
+        //                 return {
+        //                     dotList: el.dotList,
+        //                     date : el.date,
+        //                     id : el.id
+        //                 }
+        //             }
                     
-                    )
-                    setDueLIst([...mapped])
+        //             )
+        //             const mapped2 = mapped.map((el)=>{
+        //                 const a = Array.from(new Set(el.dotList.dots))
+
+        //                 return {
+        //                     dotList: a,
+        //                     date : el.date,
+        //                     id : el.id
+        //                 }
+        //             })
+        //             setDueLIst([...mapped])
                     
-                    alert(dueList.length)
-                } else{
-                    setDueLIst([...array])
-                }
+        //             alert(dueList.length)
+        //         } else{
+        //             setDueLIst([...array])
+        //         }
                 
-            }
+        //     }
+            
+        // }
+
+        for (let i = 0; i < addDue.length; i++) {
+            if(addDue[i].date == current)
+            setDIndex([...dIndex, i])
             
         }
-        
-        integrateDue([...addDue])
+        alert(dIndex[1])
+        // integrateDue([...addDue])
         // setOb({...dueOb})
-    }, [todos])
+    }, [addDue])
     
     
     const dueOb = dueList.length > 0 ?  Object.assign({}, ...dueList.map((due)=>({
